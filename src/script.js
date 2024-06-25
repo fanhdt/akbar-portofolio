efc03d8728999c93d836f24652394a1aa5c6a58f
fanhdt <mochamadirfan0211@gmail.com>
2024-06-25 11:46:23 +0700

    add animation

--- src/script.js
+++ src/script.js
@@ -56,3 +56,19 @@ document.querySelectorAll(".accordion-btn").forEach((button) => {
     }
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const btndropdown = document.getElementById("btn-dropdown");
+  const dropdownmenu = document.getElementById("dropdown-menu");
+
+  btndropdown.addEventListener("click", function () {
+    dropdownmenu.classList.toggle("hidden");
+  });
+
+  document.addEventListener("click", function (event) {
+    if (!btndropdown.contains(event.target) && !dropdownmenu.contains(event.target)) {
+      dropdownmenu.classList.add("hidden");
+    }
+  });
+});
+ 
